ec8c59396e6af0999b2f46a1070a792e60a5da40
MMikk <mm2u20@soton.ac.uk>
2024-04-14 18:57:02 +0300

    added commenting

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
 
 class CP_manager{
     // Class for keeping checkpoints in memory. Aim is to find the longest stretch on the path
@@ -59,6 +63,7 @@ bool Check_boost(vector<int> best_point, vector<int> next_point, int angle){
     I could fix it
     */
     
+    
     if (next_point == best_point and angle < 10 and angle > -10){
         return true;
     }
@@ -68,6 +73,9 @@ bool Check_boost(vector<int> best_point, vector<int> next_point, int angle){
 };
 
 string Thrust_managment(int angle, int distance){
+    // If the pod is pointing almost straight at the target then apply max thrust
+    // otherwise scale down the thrust in proprtion to how close to the target the pod is (slows down slightly)
+    // and how misaligned with the target the pod is. This stops the pod from shooting off in the wrong direction  
     string thrust;
     if (angle <= 10){
         thrust = to_string(100);
@@ -94,7 +102,11 @@ vector<int> add_vectors(vector<int> v1, vector<int> v2){
     }
 }
 
+
 vector<int> Manage_offset(int x, int y, int current_x, int current_y, int last_x, int last_y){
+    // Move the target of the pod by applying an offset
+    // A sketch of the idea is in the Readme file on git
+    // Aim is to make the pod steer more 
     int magnitude_correction = 2;
     int dx = current_x - last_x;
     int dy = current_y - last_y;
@@ -104,15 +116,16 @@ vector<int> Manage_offset(int x, int y, int current_x, int current_y, int last_x
     return CP;
 };
 
-int main(){
+int main()
+{
     CP_manager pod_CP;
     vector<int> last_turn_CP({-1,-1});  // The checkpoint that the pod was travelling towards last turn
     bool boost_available = true;  // Only check for boost once per race 
     vector<int> CP_for_boost;  // The CP to be used for boost
+    vector<int> adjusted_CP;  // A point with an offset for better steering
     int last_x;
     int last_y;
-    vector<int> adjusted_CP;
-
+    
     // game loop
     while (1) {
         int x;
@@ -124,70 +137,43 @@ int main(){
         cin >> x >> y >> next_checkpoint_x >> next_checkpoint_y >> next_checkpoint_dist >> next_checkpoint_angle; cin.ignore();
         int opponent_x;
         int opponent_y;
-        int dist_to_opp;
         cin >> opponent_x >> opponent_y; cin.ignore();
         string thrust;
         
 
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
         if (last_turn_CP[0] != next_checkpoint_x or last_turn_CP[1] != next_checkpoint_y){
-            // CP changed
+            // CP has changed. The pod is now travelling towards a new CP
             pod_CP.manage(next_checkpoint_x, next_checkpoint_y);  // 
-            cerr << "CP manager called" << endl;
             CP_for_boost = pod_CP.best_CP;
         }
 
-        // Chech if to use boost. Uses boost if last_turn_CP 
+        // Check if to use boost based on the angle and which CP the pod is travelling towards
         bool check = Check_boost(CP_for_boost, last_turn_CP, next_checkpoint_angle); 
-        if (boost_available == true and check == true){
-                boost_available = false;
-                cerr << next_checkpoint_angle << endl;
-                thrust = "BOOST";
-            }
-        else{
+    
+        if (boost_available == false){
+            // manage thrust
             thrust = Thrust_managment(next_checkpoint_angle,next_checkpoint_dist);
         }
 
-        adjusted_CP = Manage_offset(next_checkpoint_x,next_checkpoint_y,x,y,last_x,last_y);
-        // Keep info about last turn in memory
-        last_turn_CP[0] = next_checkpoint_x;
-        last_turn_CP[1] = next_checkpoint_y;
-        last_x = x;
-        last_y = y;
-        cout << adjusted_CP[0] << " " << adjusted_CP[1] << " " << thrust << endl;
-    }
-}
-
-/*
-string thrust;
-
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
-        cerr << "Debug "<< next_checkpoint_dist << endl;
-        dist_to_opp = pow(pow((x-opponent_x),2) + pow((y-opponent_y),2), 0.5);
-        if (dist_to_opp > 600 or dist_to_opp < 380){
-            if (next_checkpoint_angle > 75 or next_checkpoint_angle < -75) {
-                thrust = " 0";
-            }
-            else if  (next_checkpoint_angle < 75 and next_checkpoint_angle > 15 and next_checkpoint_dist < 3000){
-                thrust = to_string((75 - next_checkpoint_angle)*100/60);
-            }
-            else if  (next_checkpoint_angle > -75 and next_checkpoint_angle < -15 and next_checkpoint_dist < 3000){
-                thrust = to_string((75 + next_checkpoint_angle)*100/60);
-            }
-            else{
-                thrust = to_string(100);
-                if (next_checkpoint_dist < 800){
-                    thrust = to_string(next_checkpoint_dist/8);
-                }
-                if (next_checkpoint_dist > 4500){
-                    thrust = "BOOST";
-                }
-            }
+        if (boost_available == true and check == true){
+                boost_available = false;
+                thrust = "BOOST";
         }
-        else{
-            thrust = to_string(100); 
-        }    
+        
+        adjusted_CP = Manage_offset(next_checkpoint_x,next_checkpoint_y,x,y,last_x,last_y);
         // You have to output the target position
         // followed by the power (0 <= thrust <= 100)
         // i.e.: "x y thrust"
-*/
+
+        // Keep info about last turn in memory
+        last_turn_CP[0] = next_checkpoint_x;
+        last_turn_CP[1] = next_checkpoint_y;
+        // posistion of the pod last turn
+        last_x = x;  
+        last_y = y;
+
+        cout << adjusted_CP[0] << " " << adjusted_CP[1] << " " << thrust << endl;
+    }
+}
